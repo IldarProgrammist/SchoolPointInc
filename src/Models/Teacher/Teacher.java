6571d711ba2253388ba3ed2c37c1cb3f0dfb760d
ildar <UrazbakhtinIV@gmail.com>
2024-10-23 19:45:06 +0300

    add update person function

--- src/Models/Teacher/Teacher.java
+++ src/Models/Teacher/Teacher.java
@@ -1,8 +1,10 @@
-package Models;
+package Models.Teacher;
+
+import Models.Person;
 
 import java.time.LocalDate;
 
-public class Teacher  extends Person{
+public class Teacher  extends Person {
     public Teacher(int teacherId, String firstName, String lastName, String patronymic, LocalDate dateOfBirth) {
         super(teacherId, firstName, lastName, patronymic, dateOfBirth);
     }
